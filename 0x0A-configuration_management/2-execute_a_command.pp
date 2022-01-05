39a2c951a28d5e5a181516932f045535b3ea1d2d
MohamedGEDI <gedimohammed@gmail.com>
2022-01-05 01:20:49 -0800

    execute

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #execute a command
 exec {'killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin',
+  command => 'pkill -n killmenow',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  oniyif  => 'test `pgrep killmenow | wc -l` -ge 1',
 }
