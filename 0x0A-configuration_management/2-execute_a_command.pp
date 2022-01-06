985d3e2bab83554388cf3ca24ce9adebeddf5680
MohamedGEDI <gedimohammed@gmail.com>
2022-01-06 09:43:24 -0800

    fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 #execute a command
 exec {'killmenow':
-  command => 'pkill -n killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  oniyif  => 'test `pgrep killmenow | wc -l` -ge 1',
+  command => 'pkill killmenow',
+  provider=> 'shell'
 }
