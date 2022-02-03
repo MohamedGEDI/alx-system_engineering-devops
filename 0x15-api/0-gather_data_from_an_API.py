f6c946af527e436b90c4ac6a3d6534c21bb0b982
MohamedGEDI <gedimohammed@gmail.com>
2022-02-03 11:23:57 +0300

    use request and json to get data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    param = 3
+    total_todos = 0
+    done_todos = 0
+    todos_title = []
+    responce = requests.get(url=f"https://jsonplaceholder.typicode.com/users/{param}")
+    responce = responce.json()
+    name = responce.get('name')
+
+    second_responce = requests.get(url=f"https://jsonplaceholder.typicode.com/users/{param}/todos")
+    second_responce = second_responce.json()
+    for todos in second_responce:
+        total_todos += 1
+        if todos.get('completed') is True:
+            done_todos += 1
+            todos_title.append(todos.get('title', 'not found'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name,
+        done_todos,
+        total_todos
+    ))
+
+    for title in todos_title:
+        print('\t ' + title)
