592e20003a5f69d1016855efbae0d894e4e832f7
MohamedGEDI <gedimohammed@gmail.com>
2022-02-15 16:24:26 +0300

    reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+'''task 1'''
+
+
+import requests
+
+def top_ten(subreddit):
+    headers = {'User-agent': 'test'}
+    responce = requests.get('http://www.reddit.com/r/{}/hot.json'
+                            .format(subreddit),
+                            headers=headers,params={'limit': '10'})
+    for post in responce.json()['data']['children']:
+        print(post['data']['title'])
