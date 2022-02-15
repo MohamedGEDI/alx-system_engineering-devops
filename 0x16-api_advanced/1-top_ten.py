2cfaf75b21140bc92e1df2b4d8d789f98023e257
MohamedGEDI <gedimohammed@gmail.com>
2022-02-15 13:32:10 +0000

    top ten posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,14 @@
 
 import requests
 
+
 def top_ten(subreddit):
     headers = {'User-agent': 'test'}
     responce = requests.get('http://www.reddit.com/r/{}/hot.json'
                             .format(subreddit),
-                            headers=headers,params={'limit': '10'})
-    for post in responce.json()['data']['children']:
-        print(post['data']['title'])
+                            headers=headers, params={'limit': '10'})
+    if responce.status_code == 200:
+        for post in responce.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print('None')
